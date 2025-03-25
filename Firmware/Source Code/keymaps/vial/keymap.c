a976f1aed905909221e9fba922993d79a0f4d962
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2025-03-25 17:56:29 +0100

    firmeware completed

--- Firmware/Source Code/keymaps/vial/keymap.c
+++ Firmware/Source Code/keymaps/vial/keymap.c
@@ -12,7 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_NO,            KC_NO,            KC_NO,            KC_NO,   KC_NO                                 
+                 KC_NO,   KC_NO,            KC_NO,            KC_NO,            KC_NO,   KC_NO
+    ),             
 
     [2] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
